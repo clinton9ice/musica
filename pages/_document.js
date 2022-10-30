3b5399cfbd76d3756a5375e94ee42dd92a1ed61a
9ice_guy <clintonufere@gmail.com>
2022-10-30 13:15:57 +0100

    SEO Fix

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,10 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta name="copyright" content="Copyright (c) Musica (9ice__guy)" />
 
+          <meta name="googlebot" content="index, follow" />
+          <meta name="publisher" content="https://muzica.vercel.app" />
           <meta name="msapplication-tap-highlight" content="no" />
           <link rel="icon" href="/logo.svg" />
           <meta name="title" content="Musica" />
@@ -53,12 +56,23 @@ class MyDocument extends Document {
             content="https://muzica.vercel.app/Banner.png"
           />
 
-          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:card" content="summary" />
+          <meta property="twitter:site" content="@Musica" />
+          <meta property="twitter:creator" content="9ice_guy" />
           <meta property="twitter:url" content="https://muzica.vercel.app/" />
           <meta
             property="twitter:title"
             content="Musica - You #1 online musica streaming plaform"
           />
+          <meta
+            property="twitter:description"
+            content="Musica is an online music streaming platform, you have a radio feature available, you can upload your own music and add music albums to collections"
+          />
+          <meta
+            property="twitter:image"
+            content="https://muzica.app/Banner.png"
+          />
+
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="application-name" content="Musica" />
           <meta name="theme-color" content="#1e293b" />
@@ -71,14 +85,6 @@ class MyDocument extends Document {
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="#1e293b"
-          />
-          <meta
-            property="twitter:description"
-            content="Musica is an online music streaming platform, you have a radio feature available, you can upload your own music and add music albums to collections"
-          />
-          <meta
-            property="twitter:image"
-            content="https://muzica.app/Banner.png"
           />
         </Head>
         <body className="page-wrapper dark:bg-black bg-slate-50 dark:text-gray-light">
